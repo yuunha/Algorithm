e3e4a7a76901fb7b1841606ff39e180c755539cb
yuunha <114053483+yuunha@users.noreply.github.com>
2024-10-25 22:21:38 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,14 @@
+-- 코드를 입력하세요
+-- 컬럼 8개: BOARD_ID, WRITER_ID, TITLE, CONTENTS, PRICE, CREATED_DATE, STATUS, VIEWS
+-- 1. 게시글 ID, 작성자 ID, 게시글 제목, 가격, 거래상태를 조회
+-- 2. 거래상태가 SALE 이면 판매중, RESERVED이면 예약중, DONE이면 거래완료 분류하여 출력
+-- 3. 결과는 게시글 ID를 기준으로 내림차순 정렬
+SELECT BOARD_ID, WRITER_ID, TITLE , PRICE , CASE 
+WHEN STATUS ='SALE' THEN '판매중'
+WHEN STATUS='RESERVED' THEN '예약중'
+WHEN STATUS='DONE' THEN '거래완료'
+END AS STATUS_
+FROM USED_GOODS_BOARD 
+WHERE CREATED_DATE='2022-10-05'
+ORDER BY BOARD_ID DESC;
+
